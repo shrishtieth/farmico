ca113ca01240bb364b22ed895fe42f4973fa86ea
shrishti.eth <87112793+shrishtieth@users.noreply.github.com>
2025-11-17 17:03:10 +0530

    Update marketplace.sol

--- marketplace.sol
+++ marketplace.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity 0.8.28;
 
-// OpenZeppelin imports (ERC721 v5-style)
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
